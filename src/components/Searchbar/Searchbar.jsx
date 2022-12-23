12bd2927eb1b435164ddee4194d8d6c814f60702
AndreyGOIT <ae.borga@gmail.com>
2022-12-23 10:14:01 +0200

    input

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,53 +1,107 @@
-// import PropTypes from 'prop-types';
-import { Component } from 'react';
+// // import PropTypes from 'prop-types';
+// import { Component } from 'react';
 import styles from './Searchbar.module.css';
 // import { toast } from 'react-toastify';
 import { ImSearch } from 'react-icons/im';
+import { useState } from 'react';
+// import { useEffect } from 'react';
 
-export class Searchbar extends Component {
-  state = {
-    query: '',
-  };
-  handleInput = e => {
+export const Searchbar = () => {
+  const [query, setQuery] = useState('');
+  console.log(query);
+
+  const handleInput = e => {
     const { value } = e.currentTarget;
-    // console.log({ value });
-    this.setState({ query: value.toLowerCase().trim() });
-    // console.log(this.state.query);
+
+    setQuery(value.toLowerCase().trim());
   };
-  handleSubmit = e => {
-    e.preventDefault();
-    const { query } = this.state;
-    // console.log(query);
+  const handleSubmit = e => {
+    e.preventdefault();
     if (query === '') {
-      // toast.error('Input something!');
-      alert('Input some world!');
+      alert('Input some word!');
       return;
     }
-    this.props.onSubmit(query);
-    this.setState({ query: '' });
+    setQuery('');
   };
+  //   handleSubmit = e => {
+  //     e.preventDefault();
+  //     const { query } = this.state;
+  //     // console.log(query);
+  //     if (query === '') {
+  //       // toast.error('Input something!');
+  //       alert('Input some world!');
+  //       return;
+  //     }
+  //     this.props.onSubmit(query);
+  //     this.setState({ query: '' });
+  //   };
 
-  render() {
-    const { query } = this.state;
-    return (
-      <>
-        <header className={styles.Searchbar}>
-          <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
-            <button type="submit" className={styles.SearchFormButton}>
-              <ImSearch />
-            </button>
-            <input
-              className={styles.SearchFormInput}
-              type="text"
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-              value={query}
-              onChange={this.handleInput}
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <header className={styles.Searchbar}>
+        <form className={styles.SearchForm} onSubmit={handleSubmit}>
+          <button type="submit" className={styles.SearchFormButton}>
+            <ImSearch />
+          </button>
+          <input
+            className={styles.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={query}
+            onChange={handleInput}
+          />
+        </form>
+      </header>
+    </>
+  );
+};
+
+// export class Searchbar extends Component {
+//   state = {
+//     query: '',
+//   };
+//   handleInput = e => {
+//     const { value } = e.currentTarget;
+//     // console.log({ value });
+//     this.setState({ query: value.toLowerCase().trim() });
+//     // console.log(this.state.query);
+//   };
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { query } = this.state;
+//     // console.log(query);
+//     if (query === '') {
+//       // toast.error('Input something!');
+//       alert('Input some world!');
+//       return;
+//     }
+//     this.props.onSubmit(query);
+//     this.setState({ query: '' });
+//   };
+
+//   render() {
+//     const { query } = this.state;
+// return (
+//   <>
+//     <header className={styles.Searchbar}>
+//       <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
+//         <button type="submit" className={styles.SearchFormButton}>
+//           <ImSearch />
+//         </button>
+//         <input
+//           className={styles.SearchFormInput}
+//           type="text"
+//           autoComplete="off"
+//           autoFocus
+//           placeholder="Search images and photos"
+//           value={query}
+//           onChange={this.handleInput}
+//         />
+//       </form>
+//     </header>
+//   </>
+// );
+//   }
+// }
